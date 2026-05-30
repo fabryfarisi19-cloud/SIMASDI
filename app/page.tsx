ff112a1955b6c63fd25bb19d066f7812eccdae09
Abi <fabryfarisi19@gmail.com>
2026-05-31 06:20:55 +0700

    hapus halaman awal

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div style={{ padding: "30px", fontFamily: "Arial" }}>
-      <h1>SIMASDI</h1>
-      <h2>Sistem Manajemen Surat dan Disposisi</h2>
-
-      <br />
-
-      <a href="/login">
-        <button>Login</button>
-      </a>
-
-      <br />
-      <br />
-
-      <a href="/dashboard">
-        <button>Dashboard</button>
-      </a>
-
-      <br />
-      <br />
-
-      <a href="/surat-masuk">
-        <button>Surat Masuk</button>
-      </a>
-
-      <br />
-      <br />
-
-      <a href="/surat-keluar">
-        <button>Surat Keluar</button>
-      </a>
-
-      <br />
-      <br />
-
-      <a href="/disposisi">
-        <button>Disposisi</button>
-      </a>
-    </div>
-  );
+  redirect("/login");
 }
