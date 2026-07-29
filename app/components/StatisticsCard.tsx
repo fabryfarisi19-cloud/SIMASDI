83ed27ae4ea813ee75f149277db76438d0809f03
fabryfarisi19-cloud <backupumum.bapasjakbar@gmail.com>
2026-07-29 07:07:25 +0700

    Perbaiki layout Statistics Card pada dashboard mobile

--- app/components/StatisticsCard.tsx
+++ app/components/StatisticsCard.tsx
@@ -13,25 +13,24 @@ export default function StatisticsCard({
   color = "bg-blue-600",
   icon,
 }: Props) {
-  return (
-    <div
-      className={`${color} rounded-2xl shadow-lg p-6 text-white hover:scale-105 transition-all duration-300`}
-    >
-      <div className="flex items-center justify-between">
-        <div>
-          <p className="text-sm opacity-90">
-            {title}
-          </p>
-
-          <h2 className="text-5xl font-bold mt-2">
-            {value}
-          </h2>
-        </div>
-
-        <div className="text-5xl opacity-30">
-          {icon}
-        </div>
-      </div>
+return (
+  <div
+    className={`${color} rounded-2xl shadow-lg p-6 text-white hover:scale-105 transition-all duration-300 relative overflow-hidden`}
+  >
+    {/* Icon di pojok kanan atas */}
+    <div className="absolute top-5 right-5 text-4xl opacity-20">
+      {icon}
     </div>
-  );
+
+    {/* Judul */}
+    <p className="text-sm opacity-90">
+      {title}
+    </p>
+
+    {/* Angka */}
+  <h2 className="text-6xl font-extrabold tracking-tight leading-none mt-4">
+  {value}
+</h2>
+  </div>
+);
 }
